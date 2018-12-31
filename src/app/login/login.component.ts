77b7bdfe0a1dd9d3f4f2e071b286ff28398c49f5
Jorge Rivera <Jorge.Rivera0@walmart.com>
2018-12-31 15:58:12 -0600

    implement 2 way data binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = 'jrivera'
+  password = ''
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleLogin() {
+    console.log(this.username)
   }
 
 }
