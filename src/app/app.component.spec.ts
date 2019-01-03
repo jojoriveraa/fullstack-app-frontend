68131c5e94148a17ebed74f2f8438c90888fb157
Jorge Rivera <Jorge.Rivera0@walmart.com>
2019-01-03 11:50:00 -0600

    add "Invalid Credentials" text

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
-import { TestBed, async, TestBedStatic } from '@angular/core/testing';
+import { TestBed, async, TestBedStatic, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
+
+  let fixture : ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -18,20 +21,21 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+  })
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'todo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('todo');
   });
 
   it('should render the app-login component', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('app-login')).not.toBeNull();
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -7,29 +7,50 @@ import { FormsModule } from '@angular/forms';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [FormsModule],
       declarations: [LoginComponent]
     })
-      .compileComponents();
+    .compileComponents();
   }));
-
+  
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
+  
   it('should handle the login', () => {
     let consoleLogSpy = spyOn(console, 'log');
     component.handleLogin()
     expect(consoleLogSpy.calls.count()).toBe(1)
   });
+  
+  describe('should render', () => {
+    let compiled : any
+    
+    beforeEach(() => {
+      compiled = fixture.debugElement.nativeElement;
+    })
+
+    it('should render the username field', () => {
+      expect(compiled.querySelector('input[name="username"]')).not.toBeNull();
+    });
+
+    it('should render the password field', () => {
+      expect(compiled.querySelector('input[name="password"]')).not.toBeNull();
+    });
+
+    it('should render the invalid credentials text', () => {
+      expect(compiled.querySelector('small').textContent).toContain('Invalid credentials')
+    });
+  })
+
 
 });
