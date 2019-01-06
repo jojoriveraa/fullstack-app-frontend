3ca1451bf99a64a263e38e943c98ab402e7fff62
Jorge Rivera <Jorge.Rivera0@walmart.com>
2019-01-06 15:38:03 -0600

    display menu items dependeing of user logged in status

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  constructor(
+    private hardcodedAuthenticationService: HardcodedAuthenticationService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
