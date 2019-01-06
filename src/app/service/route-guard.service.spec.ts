d3e6a8e39937f968192bf3b8e92b94afd2b2795c
Jorge Rivera <Jorge.Rivera0@walmart.com>
2019-01-06 17:54:24 -0600

    Update route-guard.service.spec.ts

--- src/app/service/route-guard.service.spec.ts
+++ src/app/service/route-guard.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RouteGuardService } from './route-guard.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('RouteGuardService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({ imports: [RouterTestingModule] })
+  );
 
   it('should be created', () => {
     const service: RouteGuardService = TestBed.get(RouteGuardService);
