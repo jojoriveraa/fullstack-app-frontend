238bfc50d6cbbbbe377ac5433ebae2b9e859e252
Jorge Rivera <Jorge.Rivera0@walmart.com>
2019-01-06 03:47:37 -0600

    add todos route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'welcome/:name', component: WelcomeComponent },
+  { path: 'todos', component: ListTodosComponent },
   { path: '**', component: ErrorComponent }
 ];
 
