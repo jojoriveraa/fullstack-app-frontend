93c825b5275c1948d995e25c7cfba4a548fbb797
Jorge Rivera <Jorge.Rivera0@walmart.com>
2019-01-06 21:33:31 -0600

    Update login.component.spec.ts

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,29 +1,44 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-  const mockRouter = { navigate: jasmine.createSpy('navigate') };
+  let service: HardcodedAuthenticationService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [FormsModule, RouterTestingModule],
       declarations: [LoginComponent],
-      providers: [{ provide: Router, useValue: mockRouter }]
+      providers: [HardcodedAuthenticationService]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    service = TestBed.get(HardcodedAuthenticationService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return invalidLogin -> false when credential are right', () => {
+    spyOn(service, 'authenticate').and.returnValue(true);
+    component.handleLogin();
+    expect(component.invalidLogin).toBeFalsy();
+    expect(service.authenticate).toHaveBeenCalled();
+  });
+
+  it('should return invalidLogin -> true when credential are wrong', () => {
+    spyOn(service, 'authenticate').and.returnValue(false);
+    component.handleLogin();
+    expect(component.invalidLogin).toBeTruthy();
+    expect(service.authenticate).toHaveBeenCalled();
   });
 });
